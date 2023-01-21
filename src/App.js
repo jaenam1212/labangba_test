57b25f95685057f6299f261ad555765bc2d23a7e
padoJaenam <97040622+padoJaenam@users.noreply.github.com>
2023-01-22 01:49:05 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home" className='logo'><img src='https://datalab.labangba.com/_app/Nav/gnb_bi.png' alt='logo'/></Navbar.Brand>        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#mypage" className='bar'><img src='https://datalab.labangba.com/_app/Nav/gnb_user_fill.svg' className='mypageIcon' alt='user'/>1234님</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
     </div>
   );
 }
+
+
 
 export default App;
